3c24713b72573c871e1dcb3ef48b5af815a84830
behzadrigi <anahitarigi8@gmail.com>
2026-08-22 02:01:34 +0330

    Refactor policy structure

--- contracts/agent_permission_firewall.py
+++ contracts/agent_permission_firewall.py
@@ -1,10 +1,19 @@
 from genlayer import *
 
+class Policy:
+    max_spending: int
+    allowed_actions: str
+    requires_human_review: bool
+    version: int
+
+
 class AgentPermissionFirewall(gl.Contract):
     agents: TreeMap[str, str]
+    policies: TreeMap[str, Policy]
 
     def __init__(self):
         self.agents = TreeMap()
+        self.policies = TreeMap()
 
     @gl.public.write
     def register_agent(self, agent_id: str, owner: str):
